66c595e94d333257ada84c97e3f612bb07ae3455
araishahmad <araish.ahmadd@gmail.com>
2026-07-30 15:36:26 +0500

    Week 5 Monday: Basics of Python

---
+++ Week 5/Monday/tempCodeRunnerFile.py
@@ -0,0 +1,66 @@
+import requests
+from urllib.parse import quote
+from datetime import datetime, timedelta
+import pandas as pd
+
+# Getting dates
+today = datetime.now()
+week_ago = today - timedelta(days=7)
+
+# Formatting dates for API
+start_date = week_ago.strftime('%Y-%m-%d')
+end_date = today.strftime('%Y-%m-%d')
+
+def geoCoding(city_name):
+    url = f'https://geocoding-api.open-meteo.com/v1/search?name={quote(city_name)}&count=1'
+    res = requests.get(url)
+    data = res.json()
+
+    if 'results' not in data:
+        return None
+
+    result = data['results'][0]
+    return result['longitude'], result['latitude']
+
+def get_weather(longitude, latitude):
+    url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m'
+
+    res = requests.get(url)
+    data = res.json()
+
+    return data['current']['temperature_2m']
+
+def get_lastWeek_weather(longitude, latitude):
+    url = f'https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min&timezone=auto'
+
+    res = requests.get(url)
+    data = res.json()
+
+    return data['daily']
+
+city_name = input('Enter city name: ')
+coordinates = geoCoding(city_name)
+
+if coordinates:
+    lon, lat = coordinates
+    # current_weather = get_weather(longitude=lon, latitude=lat)
+    # print(f'Current temperature of {city_name}: {current_weather}°C')
+    weather = get_lastWeek_weather(lon, lat)
+
+    dates = weather['time']
+    max_temps = weather['temperature_2m_max']
+    min_temps = weather['temperature_2m_min']
+
+    daily_data = weather
+    df = pd.DataFrame({
+        'date': daily_data['time'],
+        'max_temp': daily_data['temperature_2m_max'],
+        'min_temp': daily_data['temperature_2m_min']
+    })
+    
+    print(f'Last week temperature of {city_name}: ')
+    for date, max_temp, min_temp in zip(dates, max_temps, min_temps):
+        print(f'{date}: max {max_temp}°C, min {min_temp}°C')
+
+else:
+    print(f'{city_name} is not a valid city')
