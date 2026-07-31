f1f1cbe03868ce276f197dd79cdd96412fa59809
araishahmad <araish.ahmadd@gmail.com>
2026-07-31 15:14:10 +0500

    Week 5 Monday: Fixed format

--- Week 5/Monday/OOP Practice/Event.py
+++ Week 5/Monday/OOP Practice/Event.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
+
 from Attendee import Attendee
+
 
 @dataclass
 class Event:
--- Week 5/Monday/OOP Practice/main.py
+++ Week 5/Monday/OOP Practice/main.py
@@ -1,10 +1,10 @@
-from Event import Event
 from Attendee import Attendee
+from Event import Event
 
-event = Event('Python OOP', '2026-07-30')
+event = Event("Python OOP", "2026-07-30")
 print(event.attendees)
 
-event.add_attendees(attendee=Attendee('Araish', 23, 'araish@gmail.com'))
+event.add_attendees(attendee=Attendee("Araish", 23, "araish@gmail.com"))
 print(event.attendees)
 
 print(event.count_attendees())
